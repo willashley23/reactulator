a0bd1c7a72a2313f9a1f8390453a4b25a971da57
willashley23 <willashley23@gmail.com>
2016-10-07 16:25:51 -0700

    Calculation container, actions, component boilerplate.

---
+++ frontend/actions/calculation_actions.js
@@ -0,0 +1,40 @@
+export const RECEIVE_CALCULATIONS = "RECEIVE_EVENTS";
+export const RECEIVE_CALCULATION = "RECEIVE_CALCULATION";
+export const REQUEST_CALCULATIONS = "REQUEST_CALCULATIONS";
+export const REQUEST_CALCULATION = "REQUEST_CALCULATION";
+export const CREATE_CALCULATION = "CREATE_CALCULATION";
+export const RECEIVE_CALCULATION_ERRORS = "RECEIVE_CALCULATION_ERRORS";
+export const CLEAR_CALCULATION_ERRORS = "CLEAR_CALCULATION_ERRORS";
+
+export const requestCalculations = () => ({
+  type: REQUEST_CALCULATIONS,
+});
+
+export const requestCalculation = id => ({
+  type: REQUEST_CALCULATION,
+  id
+});
+
+export const receiveCalculations = calculations => ({
+  type: RECEIVE_CALCULATIONS,
+  calculations
+});
+
+export const receiveCalculation = calculation => ({
+  type: RECEIVE_CALCULATION,
+  calculation
+});
+
+export const createCalculation = calculation => ({
+  type: CREATE_CALCULATION,
+  calculation
+});
+
+export const receiveCalculationErrors = (errors) => ({
+  type: RECEIVE_CALCULATION_ERRORS,
+  errors
+});
+
+export const clearCalculationErrors = () => ({
+  type: CLEAR_CALCULATION_ERRORS
+});
---
+++ frontend/components/calc/calculation_container.jsx
@@ -0,0 +1,22 @@
+import { connect } from 'react-redux';
+import {requestCalculations, receiveCalculations, createCalculation}'../../actions/calculation_actions';
+import Calculation from './calculation';
+
+
+const mapStateToProps = (state) => ({
+  return {
+    calculations: state.calculations,
+    currentUser: state.session.currentUser
+  }
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  createCalculation: (calculation) => {dispatch(createCalculation(calculation))},
+  destroyCalculation: (calculation) => {dispatch(destroyCalculation(calculation))},
+  requestCalculations: () => {dispatch(requestCalculations())}
+});
+
+export default connect (
+  mapStateToProps,
+  mapDispatchToProps
+)(Calculation);
